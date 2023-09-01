3cc1c4f724568dd40d36428fa2a419e0b923a2b4
Subhadra27 <subhadra27.sahu@gmail.com>
2023-09-01 11:50:26 +0530

    first program to Pratice branch

---
+++ src/arrays/ArrayProgram1.java
@@ -0,0 +1,15 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class ArrayProgram1 {
+
+	public static void main(String[] args) {
+		int arr[] = {20,31,12,90};
+		System.out.println(arr);
+		System.out.println(Arrays.toString(arr));
+		System.out.println(arr.getClass());
+		System.out.println(Arrays.toString(arr).getClass());
+	}
+
+}
---
+++ src/conditional/Examples.java
@@ -0,0 +1,42 @@
+package conditional;
+
+public class Examples {
+	
+	public static void main(String args[]) {
+//		int i =1;
+//		while(i<=10)
+//		{
+//			System.out.println(i);
+//		}
+//		System.out.println("-----");
+		
+//		int j=1;
+//		while(j<=50) {
+//			System.out.println(j);
+//			System.out.println("Hi");
+//			if(j%5==0) {
+//				System.out.println("Hi");
+//				break;
+//			}
+//			j++;
+//			
+//		}
+		
+		// for loop
+		
+		for(int n=1;n<=50;n++) {
+			System.out.println(n);
+			if(n%5==0) {
+				System.out.println("Bye");
+				break;
+			}
+			
+			
+			
+			
+		}
+		
+
+	}
+
+}
